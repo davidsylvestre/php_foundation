58213fdd3198e10629501d4d33458c1ab5dd0fd9
david sylvestre <david.sylvestre@hotmail.com>
2015-04-05 20:31:20 -0300

    Incluindo rodapé

--- public/index.php
+++ public/index.php
@@ -42,7 +42,7 @@
       </div>
       <footer class="footer">
          <div class="container">
-            <p class="text-muted">Todos os direitos reservados - $ano</p>
+            <p class="text-muted"><?php require_once('../view/rodape.php'); ?></p>
          </div>
       </footer>
       <!-- Bootstrap core JavaScript
