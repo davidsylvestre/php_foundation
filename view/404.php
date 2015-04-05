181689e372b61ee26ec110fc5cf5fbf02219a599
david sylvestre <david.sylvestre@hotmail.com>
2015-04-05 20:20:57 -0300

    Criado arquivos de visualização

---
+++ view/404.php
@@ -0,0 +1,4 @@
+         <div class="page-header">
+            <h1>404</h1>
+         </div>
+         <p>Pagina não encontrada!</p>
---
+++ view/produto.php
@@ -0,0 +1,9 @@
+         <div class="page-header">
+            <h1>Produtos</h1>
+         </div>
+         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, ea dolorem beatae harum quisquam nulla. Quos, quia officia animi cupiditate aperiam laudantium ipsum aspernatur dicta iste, optio sit possimus.</span>
+         <span>Maxime quam, placeat nisi accusantium dolor quia voluptates quibusdam rerum doloribus modi officiis dolore provident voluptatibus eaque animi voluptate fugiat nobis repudiandae quod eum nemo numquam, itaque, soluta dicta. Non.</span>
+         <span>Quisquam porro iure qui aliquam doloribus necessitatibus ducimus iusto harum aut odio, dolores fuga, ab accusamus labore tenetur, quas. Tempora commodi tenetur praesentium esse facere similique nulla totam quibusdam dignissimos!</span>
+         <span>Praesentium accusantium maiores officia, distinctio maxime laborum omnis. Sapiente at repellat reprehenderit blanditiis cumque itaque labore iure quis assumenda delectus, ducimus rerum dolor iusto quo quaerat sunt repudiandae, aliquid ut!</span>
+         <span>Omnis alias at, amet doloremque molestias enim quibusdam error excepturi earum magni porro veniam incidunt nihil odit quaerat repellendus, unde repellat nulla ut. Impedit vitae mollitia, aut explicabo maiores laboriosam.</span>
+         
